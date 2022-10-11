43403c5b3edc7bb84c9ee028d524dd2aafc4e0eb
Can Sacan <cansacan1406@gmail.com>
2022-10-11 18:47:17 +0200

    ilk

---
+++ Quantum_FPGA_project_1/Quantum_FPGA_project_1.srcs/sources_1/new/top_level.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11.10.2022 18:27:12
+// Design Name: 
+// Module Name: top_level
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_level(
+
+    );
+endmodule
